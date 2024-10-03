a7e0ac338c66a1fe5b4417ac9d3d485abd0d7c52
Marque Ray <mray2k4@yahoo.com>
2024-10-02 22:16:23 -0700

    Got movie list

--- movies/script.js
+++ movies/script.js
@@ -14,6 +14,7 @@ const getGenres = async () => {
     const jsonResponse = await response.json();
     const genres = jsonResponse.genres;
      return genres;
+  
    }
   } 
   catch (error) {
@@ -21,13 +22,27 @@ const getGenres = async () => {
   }
 };
 
-const getMovies = () => {
-  const selectedGenre = getSelectedGenre();
+const getMovies = async () => {
+  const selectedGenre = await getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genre=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
 
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      return movies;
+      
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
+// getMovies()
 
 const getMovieInfo = () => {
-
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
